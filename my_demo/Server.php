9aa0cfbedf202713556d455750450fd0f06c1fd9
back0893 <back0893@163.com>
2018-11-29 13:59:16 +0800

    整理下

--- my_demo/Server.php
+++ my_demo/Server.php
@@ -13,17 +13,23 @@ class Server
     protected $is_stop=false;
     protected $count=2;
     protected $main_socket;
-
+    protected $back=false;
+    protected $pid_path;
+    protected $user='root';
+    protected $group='root';
     public function __construct()
     {
-        $this->main_pid=posix_getpid();
-        $this->main_socket=stream_socket_server("tcp://0.0.0.1:5000",$errno,$errmsg);
+        $this->main_socket=stream_socket_server("tcp://0.0.0.0:5000",$errno,$errmsg);
         if(!$this->main_socket){
             exit("错误编号:{$errno},错误信息:{$errmsg}".PHP_EOL);
         }
+        $this->pid_path=__DIR__.'/pid.log';
         $this->installSign();
     }
 
+    /**
+     * 生成子进程
+     */
     public function forkSon(){
         while(count($this->son_pid)<$this->count){
             $pid=pcntl_fork();
@@ -40,12 +46,16 @@ class Server
             }
         }
     }
+
+    /**
+     * 子进程监听
+     */
     public function listen(){
         $reads=[$this->main_socket];
-        $write=[];
+        $writes=[];
         while(1){
             $tmp_reads=$reads;
-            $tmp_write=$write;
+            $tmp_write=$writes;
             $r=@stream_select($tmp_reads,$tmp_write,$except,null);
             if($r===false){
                 exit('!!!读取失败!!!'.PHP_EOL);
@@ -53,19 +63,19 @@ class Server
             foreach ($tmp_reads as $read){
                 if($read===$this->main_socket){
                     $rev=stream_socket_accept($read,5,$remote_address);
-                    $tmp_reads[]=$rev;
-                    $tmp_write=$rev;
+                    $reads[]=$rev;
+                    $writes[]=$rev;
                 }
                 else{
                     $message=trim(fread($read,65535));
                     if(strlen($message)==0){
-                        $index=array_search($read,$tmp_reads);
+                        $index=array_search($read,$reads);
                         if($index!==false && $index>=0){
-                            unset($tmp_reads[$index]);
+                            unset($reads[$index]);
                         }
-                        $index=array_search($read,$tmp_write);
+                        $index=array_search($read,$writes);
                         if($index!==false && $index>=0){
-                            unset($tmp_write[$index]);
+                            unset($writes[$index]);
                         }
                         fclose($read);
                     }
@@ -79,10 +89,19 @@ class Server
             }
         }
     }
+
+    /**
+     * 安装信号
+     */
     public function installSign(){
         pcntl_async_signals(true);
         pcntl_signal(SIGINT,[$this,'installSIGINT'],false);
+        pcntl_signal(SIGPIPE, SIG_IGN, false);
     }
+
+    /**
+     * 信号处理
+     */
     public function installSIGINT(){
         $pid=posix_getpid();
         if($pid!=$this->main_pid){
@@ -93,7 +112,13 @@ class Server
             $this->is_stop=true;
         }
     }
+
+    /**
+     * 守护的父进程,用来管理子进程
+     *  重启或者关闭
+     */
     public function domain(){
+        $this->main_pid=posix_getpid();
         while(1){
             $status=0;
             $son_pid=pcntl_wait($status,WUNTRACED);
@@ -109,16 +134,122 @@ class Server
             }
         }
     }
+
+    /**
+     * 退出全部清理
+     */
     public function stopAll(){
-        foreach ($this->son_pid as $pid){
-            posix_kill($pid,SIGINT);
+        if($this->main_pid==posix_getpid()){
+            foreach ($this->son_pid as $pid){
+                posix_kill($pid,SIGINT);
+            }
+            sleep(1);
+            echo "主进程退出".PHP_EOL;
+            exit(0);
+        }else{
+            echo "子进程退出".PHP_EOL;
+            exit(0);
         }
-        sleep(1);
-        echo "主进程退出,子进程退出".PHP_EOL;
-        exit(0);
+
+    }
+
+    /**
+     * 解析shell 命令
+     */
+    public function parseShell(){
+        global $argv;
+        array_shift($argv);
+        $order=array_shift($argv);
+        $ext=array_shift($argv);
+        if($ext==='-d'){
+            $this->back=true;
+        }
+        $master_pid=$this->getMasterPid();
+        if($this->masterIsAlive($master_pid)){
+            if($order=='start'){
+                exit("服务器已经启动\n");
+            }elseif($order=='stop'){
+                exit("服务器还未启动\n");
+            }
+        }
+        switch ($order){
+            case "start":
+                break;
+            case 'stop':
+                posix_kill($master_pid,SIGINT);
+                exit();
+                break;
+        }
+    }
+    public function getMasterPid(){
+        if(file_exists($this->pid_path) && is_readable($this->pid_path)){
+            $master_pid=file_get_contents($this->pid_path);
+        }else{
+            $master_pid=0;
+        }
+        return $master_pid;
+    }
+    public function masterIsAlive($master_pid){
+        if($master_pid){
+            return posix_kill($master_pid,SIG_DFL) && ($master_pid !=posix_getpid());
+        }
+        return false;
+    }
+    public function back(){
+        if(!$this->back) return ;
+        $this->setUserAndGroup();
+        umask(0);
+        $pid=pcntl_fork();
+        if($pid>0){
+            exit(0);
+        }elseif($pid==-1){
+            exit("创建守护进程失败!!--1\n");
+        }
+        $sid=posix_setsid();
+        if($sid==-1){
+            exit("创建守护进程失败!!--2\n");
+        }
+        $pid=pcntl_fork();
+        if($pid>0){
+            exit(0);
+        }elseif($pid==-1){
+            exit("创建守护进程失败!!--3\n");
+        }
+        $this->saveMasterPid();
+    }
+    public function saveMasterPid(){
+        $this->main_pid=posix_getpid();
+        if($this->back){
+            file_put_contents($this->pid_path,$this->main_pid);
+        }
     }
     public function run(){
+        $this->parseShell();
+        $this->back();
         $this->forkSon();
         $this->domain();
     }
+    public function setUserAndGroup(){
+        $user_info = posix_getpwnam($this->user);
+        if(!$user_info){
+            exit('选择的用户不存在!!'.PHP_EOL);
+        }
+        $uid=$user_info['uid'];
+        $gid=$user_info['gid'];
+        if($this->group){
+            $group_info=posix_getgrnam($this->group);
+            if(!$group_info){
+                exit('选择的用户组不存在!!'.PHP_EOL);
+            }
+            $gid=$group_info['gid'];
+        }
+        if($uid!=posix_getuid() || $gid!=posix_getgid()){
+            if(!posix_setgid($gid) || !posix_initgroups($user_info['name'],$gid) || !posix_setuid($uid)){
+                exit("改变用户,用户组失败!".PHP_EOL);
+            }
+        }
+    }
 }
+
+$server=new Server();
+$server->run();
---
+++ my_demo/singel_server.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liu
+ * Date: 2018/11/22
+ * Time: 1:27
+ * 一个简单的demo tcp服务器 php实现
+ */
+
+$socket=stream_socket_server("tcp://0.0.0.0:8000",$errno,$errstr);
+if(!$socket){
+    exit('server up fail!');
+}
+$reads=[$socket];
+$writes=[];
+while(1){
+    $tmp_read=$reads;
+    $tmp_write=$writes;
+    //如果没有就阻塞
+    $r=stream_select($tmp_read,$tmp_write,$tmp_error,null);
+    if($r===false){
+       exit('失败!');
+    }
+    echo '===>read';
+    foreach ($tmp_read as $read){
+        if($read===$socket){
+            $sock=stream_socket_accept($socket,5,$remote_address);
+            echo '有新的链接上来了'.$remote_address;
+            $reads[]=$sock;
+            $writes[]=$sock;
+        }
+        else{
+            $string=trim(fread($read,65535));
+            echo "发送的数据是:{$string}";
+            if(in_array($read,$tmp_write)){
+                //可写
+                fwrite($read,'我受到了');
+            }
+            if(strlen($string)){
+                fwrite($read,'关闭!');
+                fclose($read);
+                $index=array_search($read,$reads,true);
+                unset($reads[$index]);
+                $index=array_search($read,$writes,true);
+                unset($writes[$index]);
+            }
+
+        }
+    }
+}
+
